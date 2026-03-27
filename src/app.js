f8011c4344ab808cce6fe3c2c86d42b25177cc3d
Komal Vyas <shivangi.vyas0804@gmail.com>
2026-03-27 06:24:15 +0530

    wildcard- Error-handling

--- src/app.js
+++ src/app.js
@@ -1,34 +1,19 @@
 const express = require('express');
 const app = express(); 
-const { adminAuth, userAuth } = require('./middlewares/auth');
 
-
-// Handle Auth Middleware for all GET POST, .. requests
-app.use("/admin", adminAuth );
-app.use("/user", userAuth);
-
-app.post("/user/login", (req, res) => {
-  res.send('user logged In.');
+app.get("/getUserData", (req, res) => {
+ // try{
+  throw new Error("qwerty");
+  res.send('user data fetched.');
+  // } catch (err) {
+  //   res.status(500).send("something went wrong. Contact support team.");
+  // }
 });
 
-app.get("/admin/getAllData", (req, res) => {
-  res.send("Got all user data.");
-});
-
-app.post("/admin/update", (req, res) => {
-  res.send("Updated admin data.");
-});
-
-app.get("/user", userAuth, (req, res) => {
-  res.send('user Data Sent.');
-});
-
-app.post("/user/update", userAuth, (res, req) => {
-  res.send('user data updated');
-});
-
-app.delete("/user/delete", userAuth, (req, res) => {
-  res.send('user deleted');
+app.use("/", (err, req, res, next) => {
+  if(err){
+    res.status(500).send("something went wrong");
+  }
 });
 
 app.listen(8080, () => {
